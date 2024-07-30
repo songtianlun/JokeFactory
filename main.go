0f1de0b5270c58174904d1a1c3305ba1e610c388
songtianlun <tianlun.song@foxmail.com>
2024-07-31 01:39:44 +0800

    add ads

--- main.go
+++ main.go
@@ -22,8 +22,8 @@ func main() {
 	http.HandleFunc("/joke", getJoke)
 	http.HandleFunc("/explain", explainJoke)
 
-	fmt.Println("Server is running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server is running on http://0.0.0.0:8080")
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
 
 func serveHTML(w http.ResponseWriter, r *http.Request) {
